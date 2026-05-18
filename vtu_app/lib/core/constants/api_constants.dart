b1e11454d098bb2c2283e5101972a38c36378196
Shola20111 <bigt4prigress20111@gmail.com>
2026-05-18 12:54:04 +0100

    updating some features in order to be tested on phone

--- vtu_app/lib/core/constants/api_constants.dart
+++ vtu_app/lib/core/constants/api_constants.dart
@@ -51,8 +51,7 @@ class ApiConstants {
 
     // static const String baseUrl = 'http://localhost:5000/api';
     
-static const String baseUrl =
-    'https://vtu-new.onrender.com/api';
+static const String baseUrl = 'https://vtu-new.onrender.com/api';
     
   
 
